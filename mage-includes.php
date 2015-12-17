fecfafcdf86f1fc12738fc0fcf6023faecdfc008
Kate Fawl <katya.fawl@gmail.com>
2015-12-17 15:28:33 +0000

    Add support for an alternative repo structure

--- mage-includes.php
+++ mage-includes.php
@@ -2,11 +2,21 @@
 $ds = DIRECTORY_SEPARATOR;
 $bp = dirname(dirname(dirname(__DIR__)));
 
+if (!file_exists($bp.$ds.'app')) {
+    foreach (array('pub', 'public') as $pubDir) {
+        if (file_exists($bp . $ds . $pubDir)) {
+            $bp .= $ds . $pubDir;
+            break;
+        }
+    }
+}
+
 $paths = array(
     $bp . $ds . 'app' . $ds . 'code' . $ds . 'local',
     $bp . $ds . 'app' . $ds . 'code' . $ds . 'community',
     $bp . $ds . 'app' . $ds . 'code' . $ds . 'core',
     $bp . $ds . 'lib',
+    $bp . $ds
     get_include_path()
 );
 
